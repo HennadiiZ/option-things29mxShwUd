a020a5cfaf41fd21e166cbd81d7e9776eed0f162
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-31 10:09:06 +0200

    inits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import './App.css';
 import React, { useState } from 'react';
-import Todos from './components/Todos';
-// import Modal from './components/Modal';
-// import Backdrop from './components/Backdrop';
 
 function App() {
-  
-  const openModalHandler = () => {};
-
-  const chooseMessage = (text) => {
-    console.log(text);
-  };
 
   return (
     <React.Fragment>
-      <Todos text="eat borsh" chooseMessage={chooseMessage}/>
-      <Todos text="eat potato" chooseMessage={chooseMessage}/>
-      <Todos text="eat me" chooseMessage={chooseMessage}/>
-      <button onClick={openModalHandler}>show</button>
+      <p>Hello</p>
     </React.Fragment>
   );
 }
--- src/components/Backdrop.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function Backdrop(props) {
-    return <div className='backdrop' onClick={props.onClose}></div>;
-};
-
-export default Backdrop;
