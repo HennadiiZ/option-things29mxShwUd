3e35e2d8a091231ba5ad3e9258e5351dad9c5bc8
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-31 11:54:05 +0200

    layout folder and MainNavigationPage

--- src/App.js
+++ src/App.js
@@ -5,23 +5,27 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import AllMeetupsPage from './pages/AllMeetups';
 import NewMeetupPage from './pages/NewMeetup';
 import FavoritesPage from './pages/Favorites';
+import MainNavigationPage from './Layout/MainNavigation';
 
 function App() {
 
   return (
-    <main>
-      <Switch>
-        <Route path='/' exact={true}>
-          <AllMeetupsPage />
-        </Route>
-        <Route path='/new-meetup'>
-          <NewMeetupPage />
-        </Route>
-        <Route path='/favorites'>
-          <FavoritesPage />
-        </Route>
-      </Switch>
-    </main>
+    <div>
+      <MainNavigationPage />
+      <main>
+        <Switch>
+          <Route path='/' exact={true}>
+            <AllMeetupsPage />
+          </Route>
+          <Route path='/new-meetup'>
+            <NewMeetupPage />
+          </Route>
+          <Route path='/favorites'>
+            <FavoritesPage />
+          </Route>
+        </Switch>
+      </main>  
+    </div>
   );
 }
 
