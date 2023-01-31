b930aa7e6102d0529e49096da1c73df7c8c9a83a
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-01-31 09:58:12 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import Todos from './components/Todos';
+// import Modal from './components/Modal';
+// import Backdrop from './components/Backdrop';
 
 function App() {
+  
+  const openModalHandler = () => {};
+
+  const chooseMessage = (text) => {
+    console.log(text);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Todos text="eat borsh" chooseMessage={chooseMessage}/>
+      <Todos text="eat potato" chooseMessage={chooseMessage}/>
+      <Todos text="eat me" chooseMessage={chooseMessage}/>
+      <button onClick={openModalHandler}>show</button>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Backdrop.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Backdrop(props) {
+    return <div className='backdrop' onClick={props.onClose}></div>;
+};
+
+export default Backdrop;
