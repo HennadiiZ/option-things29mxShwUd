51880f42bd7ffdcbe191f136b3276efd862fc813
Hennadii Zhukov <hennadii.zhukov@jetsoftpro@com>
2023-02-02 13:50:10 +0200

    Favorites update

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,10 +1,17 @@
 import { useContext } from 'react';
 import DataContext from '../_store/data-context';
 
+import MeetupList from '../components/MeetupList/MeetupList';
+
 const FavoritesPage = () => {
   const favCtx = useContext(DataContext);
 
-  return <p>Favorites</p>
+  return (
+    <section>
+      <h1>My Fav</h1>
+      <MeetupList meetups={favCtx.favorites}/>
+    </section>
+  );
 };
 
 export default FavoritesPage;
